fd99ae304c78ad05cd3a7c5e1ee5c229e43b913a
TheKing4012 <rick40709@gmail.com>
2024-05-05 18:18:54 +0200

    WIP

---
+++ app/src/main/java/com/example/NotificationActivity.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_notification);
+        textView = findViewById(R.id.textViewData);
+        String data = getIntent().getStringExtra("data"); // getCharSequenceArrayListExtra
+        textView.setText(data);
+    }
+}
